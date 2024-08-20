e819d0193bc8708bee30c208c295f59fa97cde60
StringNick <stringnickq@gmail.com>
2024-08-20 17:16:48 +0200

    rm test

--- src/mint/lightning/invoices/invoice.zig
+++ src/mint/lightning/invoices/invoice.zig
@@ -618,13 +618,6 @@ pub const Fallback = union(enum) {
     script_hash: [20]u8,
 };
 
-test "states" {
-    const s = States.parse_n;
-
-    const st = try s.nextState('3');
-    std.log.debug("dd {any}", .{st});
-}
-
 test "decode" {
     const str = "lnbc1pvjluezpp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqdpl2pkx2ctnv5sxxmmwwd5kgetjypeh2ursdae8g6twvus8g6rfwvs8qun0dfjkxaq8rkx3yf5tcsyz3d73gafnh3cax9rn449d9p5uxz9ezhhypd0elx87sjle52x86fux2ypatgddc6k63n7erqz25le42c4u4ecky03ylcqca784w";
 
