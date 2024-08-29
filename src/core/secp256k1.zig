0dc38f784e7ef38c3ccc7767b4ec56fb3b42d2f0
StringNick <stringnickq@gmail.com>
2024-08-29 17:28:45 +0200

    RIPEMD 160 BIP32 secp256k1 updates

--- src/core/secp256k1.zig
+++ src/core/secp256k1.zig
@@ -1,28 +1,122 @@
 const std = @import("std");
+
 const secp256k1 = @cImport({
     @cInclude("secp256k1.h");
     @cInclude("secp256k1_recovery.h");
     @cInclude("secp256k1_preallocated.h");
+    @cInclude("secp256k1_schnorrsig.h");
 });
 
 const crypto = std.crypto;
 
-pub const Secp256k1 = struct {
-    ctx: ?*secp256k1.struct_secp256k1_context_struct,
-    ptr: []align(16) u8,
+pub const KeyPair = struct {
+    inner: secp256k1.secp256k1_keypair,
 
-    pub fn deinit(self: @This(), allocator: std.mem.Allocator) void {
-        allocator.free(@as([]align(16) u8, @ptrCast(self.ptr)));
+    /// Creates a [`KeyPair`] directly from a Secp256k1 secret key.
+    pub fn fromSecretKey(secp: *const Secp256k1, sk: *const SecretKey) !KeyPair {
+        var kp = secp256k1.secp256k1_keypair{};
+
+        if (secp256k1.secp256k1_keypair_create(secp.ctx, &kp, &sk.data) != 1) {
+            @panic("the provided secret key is invalid: it is corrupted or was not produced by Secp256k1 library");
+        }
+
+        return .{ .inner = kp };
+    }
+};
+
+pub const XOnlyPublicKey = struct {
+    inner: secp256k1.secp256k1_xonly_pubkey,
+
+    /// Creates a schnorr public key directly from a slice.
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::InvalidPublicKey`] if the length of the data slice is not 32 bytes or the
+    /// slice does not represent a valid Secp256k1 point x coordinate.
+    pub inline fn fromSlice(data: []const u8) !XOnlyPublicKey {
+        if (data.len == 0 or data.len != 32) {
+            return error.InvalidPublicKey;
+        }
+
+        var pk: secp256k1.secp256k1_xonly_pubkey = undefined;
+
+        if (secp256k1.secp256k1_xonly_pubkey_parse(
+            secp256k1.secp256k1_context_no_precomp,
+            &pk,
+            data.ptr,
+        ) == 1) {
+            return .{ .inner = pk };
+        }
+
+        return error.InvalidPublicKey;
     }
 
-    pub fn genNew(allocator: std.mem.Allocator) !@This() {
-        // verify and sign only
-        const size: usize = secp256k1.secp256k1_context_preallocated_size(257 | 513);
+    /// Serializes the key as a byte-encoded x coordinate value (32 bytes).
+    pub inline fn serialize(self: XOnlyPublicKey) [32]u8 {
+        var ret: [32]u8 = undefined;
 
-        const ptr = try allocator.alignedAlloc(u8, 16, size);
+        const err = secp256k1.secp256k1_xonly_pubkey_serialize(
+            secp256k1.secp256k1_context_no_precomp,
+            &ret,
+            &self.inner,
+        );
+        std.debug.assert(err == 1);
+        return ret;
+    }
 
+    /// Creates a [`PublicKey`] using the key material from `pk` combined with the `parity`.
+    pub fn publicKey(pk: XOnlyPublicKey, parity: enum {
+        even,
+        odd,
+    }) !PublicKey {
+        var buf: [33]u8 = undefined;
+
+        // First byte of a compressed key should be `0x02 AND parity`.
+        buf[0] = switch (parity) {
+            .even => 0x02,
+            .odd => 0x03,
+        };
+
+        buf[1..33].* = pk.serialize();
+
+        return PublicKey.fromSlice(&buf) catch @panic("buffer is valid");
+    }
+};
+
+pub const Secp256k1 = struct {
+    ctx: ?*secp256k1.struct_secp256k1_context_struct,
+
+    pub fn deinit(self: @This()) void {
+        secp256k1.secp256k1_context_preallocated_destroy(self.ctx);
+    }
+
+    /// Verifies a schnorr signature.
+    pub fn verifySchnorr(
+        self: *Secp256k1,
+        sig: Signature,
+        msg: [32]u8,
+        pubkey: XOnlyPublicKey,
+    ) !void {
+        if (secp256k1.secp256k1_schnorrsig_verify(
+            self.ctx,
+            &sig.inner,
+            &msg,
+            32,
+            &pubkey.inner,
+        ) != 1) return error.InvalidSignature;
+    }
+
+    pub fn signSchnorrHelper(self: *const Secp256k1, msg: [32]u8, keypair: KeyPair, nonce_data: []const u8) !Signature {
+        var sig: [64]u8 = undefined;
+
+        std.debug.assert(1 == secp256k1.secp256k1_schnorrsig_sign(self.ctx, (&sig).ptr, &msg, &keypair.inner, nonce_data.ptr));
+
+        return .{ .inner = sig };
+    }
+
+    pub fn genNew() !@This() {
         const ctx =
-            secp256k1.secp256k1_context_preallocated_create(ptr.ptr, 257 | 513);
+            secp256k1.secp256k1_context_create(257 | 513);
 
         var seed: [32]u8 = undefined;
 
@@ -34,7 +128,6 @@ pub const Secp256k1 = struct {
 
         return .{
             .ctx = ctx,
-            .ptr = ptr,
         };
     }
 };
@@ -106,6 +199,10 @@ pub const Scalar = struct {
 pub const PublicKey = struct {
     pk: secp256k1.secp256k1_pubkey,
 
+    pub fn eql(self: PublicKey, other: PublicKey) bool {
+        return std.mem.eql(u8, &self.pk.data, &other.pk.data);
+    }
+
     // json serializing func
     pub fn jsonStringify(self: PublicKey, out: anytype) !void {
         try out.write(std.fmt.bytesToHex(&self.serialize(), .lower));
@@ -124,6 +221,44 @@ pub const PublicKey = struct {
         }
     }
 
+    /// Returns the [`XOnlyPublicKey`] (and it's [`Parity`]) for this [`PublicKey`].
+    pub inline fn xOnlyPublicKey(self: *const PublicKey) !struct { XOnlyPublicKey, enum { even, odd } } {
+        var pk_parity: i32 = 0;
+        var xonly_pk = secp256k1.secp256k1_xonly_pubkey{};
+        const ret = secp256k1.secp256k1_xonly_pubkey_from_pubkey(
+            secp256k1.secp256k1_context_no_precomp,
+            &xonly_pk,
+            &pk_parity,
+            &self.pk,
+        );
+
+        std.debug.assert(ret == 1);
+
+        return .{
+            .{ .inner = xonly_pk },
+            if (pk_parity & 1 == 0) .even else .odd,
+        };
+    }
+
+    /// Verify schnorr signature
+    pub fn verify(self: *const PublicKey, secp: *Secp256k1, msg: []const u8, sig: Signature) !void {
+        var hasher = crypto.hash.sha2.Sha256.init(.{});
+        hasher.update(msg);
+
+        const hash = hasher.finalResult();
+
+        try secp.verifySchnorr(sig, hash, (try self.xOnlyPublicKey())[0]);
+    }
+
+    /// [`PublicKey`] from hex string
+    pub fn fromString(s: []const u8) !@This() {
+        var buf: [100]u8 = undefined;
+        const decoded = try std.fmt.hexToBytes(&buf, s);
+
+        return PublicKey.fromSlice(decoded);
+    }
+
+    /// [`PublicKey`] from bytes slice
     pub fn fromSlice(c: []const u8) !@This() {
         var pk: secp256k1.secp256k1_pubkey = .{};
 
@@ -152,6 +287,14 @@ pub const PublicKey = struct {
         return ret;
     }
 
+    /// Serializes the key as a byte-encoded pair of values, in uncompressed form.
+    pub inline fn serializeUncompressed(self: PublicKey) [65]u8 {
+        var ret = [_]u8{0} ** 65;
+
+        self.serializeInternal(&ret, 2);
+        return ret;
+    }
+
     inline fn serializeInternal(self: PublicKey, ret: []u8, flag: u32) void {
         var ret_len = ret.len;
 
@@ -177,6 +320,24 @@ pub const PublicKey = struct {
         return error.InvalidTweak;
     }
 
+    /// Tweaks a [`PublicKey`] by adding `tweak * G` modulo the curve order.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the resulting key would be invalid.
+    pub inline fn addExpTweak(
+        self: *const PublicKey,
+        secp: Secp256k1,
+        tweak: Scalar,
+    ) !PublicKey {
+        var s = self.*;
+        if (secp256k1.secp256k1_ec_pubkey_tweak_add(secp.ctx, &s.pk, &tweak.data) == 1) {
+            return s;
+        } else {
+            return error.InvalidTweak;
+        }
+    }
+
     pub fn combine(self: @This(), other: PublicKey) !PublicKey {
         return PublicKey.combineKeys(&.{
             &self, &other,
@@ -200,8 +361,60 @@ pub const PublicKey = struct {
     }
 };
 
+pub const Signature = struct {
+    inner: [64]u8,
+
+    pub fn fromString(s: []const u8) !Signature {
+        if (s.len / 2 > 64) return error.InvalidSignature;
+        var res = [_]u8{0} ** 64;
+
+        _ = try std.fmt.hexToBytes(&res, s);
+        return .{ .inner = res };
+    }
+
+    pub fn toString(self: Signature) [128]u8 {
+        return std.fmt.bytesToHex(&self.inner, .lower);
+    }
+};
+
 pub const SecretKey = struct {
     data: [32]u8,
+
+    /// Generate random [`SecretKey`]
+    pub fn generate() SecretKey {
+        var rng = std.Random.DefaultPrng.init(@intCast(std.time.timestamp()));
+
+        var d: [32]u8 = undefined;
+
+        while (true) {
+            rng.fill(&d);
+            if (SecretKey.fromSlice(&d)) |sk| return sk else |_| continue;
+        }
+    }
+
+    /// Schnorr Signature on Message
+    pub fn sign(self: *const SecretKey, msg: []const u8) !Signature {
+        var hasher = crypto.hash.sha2.Sha256.init(.{});
+        hasher.update(msg);
+
+        const hash = hasher.finalResult();
+
+        var secp = try Secp256k1.genNew();
+        defer secp.deinit();
+
+        var rng = std.Random.DefaultPrng.init(@intCast(std.time.timestamp()));
+
+        var aux: [32]u8 = undefined;
+        rng.fill(&aux);
+
+        return secp.signSchnorrHelper(hash, try KeyPair.fromSecretKey(&secp, self), &aux);
+    }
+
+    pub fn fromString(data: []const u8) !@This() {
+        var buf: [100]u8 = undefined;
+
+        return SecretKey.fromSlice(try std.fmt.hexToBytes(&buf, data));
+    }
 
     pub fn fromSlice(data: []const u8) !@This() {
         if (data.len != 32) {
@@ -228,5 +441,52 @@ pub const SecretKey = struct {
 
     pub fn toString(self: @This()) [32 * 2]u8 {
         return std.fmt.bytesToHex(&self.data, .lower);
+    }
+
+    /// Tweaks a [`SecretKey`] by multiplying by `tweak` modulo the curve order.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the resulting key would be invalid.
+    pub inline fn mulTweak(self: *const SecretKey, tweak: Scalar) !SecretKey {
+        var s = self.*;
+        if (secp256k1.secp256k1_ec_seckey_tweak_mul(
+            secp256k1.secp256k1_context_no_precomp,
+            &s.data,
+            &tweak.data,
+        ) != 1) {
+            return error.InvalidTweak;
+        }
+
+        return s;
+    }
+
+    /// Tweaks a [`SecretKey`] by adding `tweak` modulo the curve order.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the resulting key would be invalid.
+    pub inline fn addTweak(self: *const SecretKey, tweak: Scalar) !SecretKey {
+        var s = self.*;
+        if (secp256k1.secp256k1_ec_seckey_tweak_add(
+            secp256k1.secp256k1_context_no_precomp,
+            &s.data,
+            &tweak.data,
+        ) != 1) {
+            return error.InvalidTweak;
+        } else {
+            return s;
+        }
+    }
+
+    pub fn jsonStringify(self: *const SecretKey, out: anytype) !void {
+        try out.write(self.toString());
+    }
+
+    pub fn jsonParse(_: std.mem.Allocator, source: anytype, _: std.json.ParseOptions) !@This() {
+        return switch (try source.next()) {
+            .string, .allocated_string => |hex_sec| SecretKey.fromString(hex_sec) catch return error.UnexpectedToken,
+            else => return error.UnexpectedToken,
+        };
     }
 };
--- src/mint/lightning/invoices/ripemd160.zig
+++ src/mint/lightning/invoices/ripemd160.zig
@@ -1,163 +1,263 @@
 const std = @import("std");
+const testing = std.testing;
+const mem = std.mem;
 
 pub const Ripemd160 = struct {
-    bytes: [160 / 8]u8,
+    const Self = @This();
+    pub const block_length = 64;
+    pub const digest_length = 20;
+    pub const Options = struct {};
 
-    pub fn hash(str: []const u8) Ripemd160 {
-        var res: Ripemd160 = undefined;
-        var temp: [5]u32 = .{ 0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0 };
-        var ptr = str.ptr;
+    s: [5]u32,
+    // Streaming Cache
+    buf: [64]u8 = undefined,
+    buf_len: u8 = 0,
+    total_len: u64 = 0,
 
-        { // compress string in 16-word chunks
-            var i = str.len / 64;
-            while (i > 0) : (i -= 1) {
-                var chunk: [16]u32 = [_]u32{0} ** 16;
-                var j: usize = 0;
-                while (j < 16) : (j += 1) {
-                    chunk[j] = std.mem.readInt(u32, ptr[0..4], .little);
-                    ptr += 4;
-                }
-                compress(&temp, chunk);
-            }
+    pub fn init(options: Options) Self {
+        _ = options;
+        return Self{
+            .s = [_]u32{
+                0x67452301,
+                0xEFCDAB89,
+                0x98BADCFE,
+                0x10325476,
+                0xC3D2E1F0,
+            },
+        };
+    }
+
+    pub fn update(d: *Self, b: []const u8) void {
+        var off: usize = 0;
+
+        // Partial buffer exists from previous update. Copy into buffer then hash.
+        if (d.buf_len != 0 and d.buf_len + b.len >= 64) {
+            off += 64 - d.buf_len;
+            @memcpy(d.buf[d.buf_len..][0..off], b[0..off]);
+
+            d.round(&d.buf);
+            d.buf_len = 0;
         }
 
-        { // compress remaining string, (str.len % 64) bytes
-            var chunk: [16]u32 = [_]u32{0} ** 16;
-            var i: usize = 0;
-            while (i < (str.len & 63)) : (i += 1) {
-                chunk[i >> 2] ^= (@as(u32, @intCast(ptr[0])) << @as(u5, @intCast(8 * (i & 3))));
-                ptr += 1;
-            }
-
-            chunk[(str.len >> 2) & 15] ^= @as(u32, 1) << @as(u5, @intCast(8 * (str.len & 3) + 7));
-
-            if ((str.len & 63) > 55) {
-                compress(&temp, chunk);
-                chunk = [_]u32{0} ** 16;
-            }
-
-            chunk[14] = @intCast(str.len << 3);
-            chunk[15] = @intCast((str.len >> 29) | (0 << 3));
-            compress(&temp, chunk);
+        // Full middle blocks.
+        while (off + 64 <= b.len) : (off += 64) {
+            d.round(b[off..][0..64]);
         }
 
-        { // write final hash
-            var i: usize = 0;
-            while (i < 160 / 8) : (i += 4) {
-                std.mem.writeInt(u32, @as(*[4]u8, @ptrCast(&res.bytes[i])), temp[i >> 2], .little);
-            }
+        // Copy any remainder for next pass.
+        const b_slice = b[off..];
+        @memcpy(d.buf[d.buf_len..][0..b_slice.len], b_slice);
+        d.buf_len += @as(u8, @intCast(b[off..].len));
+
+        d.total_len += b.len;
+    }
+
+    fn blockToWords(block: *const [block_length]u8) [16]u32 {
+        var words: [16]u32 = undefined;
+        for (words, 0..) |_, i| {
+            // zig fmt: off
+            words[i] = 0;
+            words[i] |= (@as(u32, block[i * 4 + 3]) << 24);
+            words[i] |= (@as(u32, block[i * 4 + 2]) << 16);
+            words[i] |= (@as(u32, block[i * 4 + 1]) << 8);
+            words[i] |= (@as(u32, block[i * 4 + 0]) << 0);
+            // zig fmt: on
+        }
+        return words;
+    }
+
+    fn func(j: usize, x: u32, y: u32, z: u32) u32 {
+        return switch (j) {
+            // f(j, x, y, z) = x XOR y XOR z                (0 <= j <= 15)
+            0...15 => x ^ y ^ z,
+            // f(j, x, y, z) = (x AND y) OR (NOT(x) AND z)  (16 <= j <= 31)
+            16...31 => (x & y) | (~x & z),
+            // f(j, x, y, z) = (x OR NOT(y)) XOR z          (32 <= j <= 47)
+            32...47 => (x | ~y) ^ z,
+            // f(j, x, y, z) = (x AND z) OR (y AND NOT(z))  (48 <= j <= 63)
+            48...63 => (x & z) | (y & ~z),
+            // f(j, x, y, z) = x XOR (y OR NOT(z))          (64 <= j <= 79)
+            // !!! omg xor and or 64
+            64...79 => x ^ (y | ~z),
+            else => unreachable,
+        };
+    }
+
+    fn round(d: *Self, b: *const [block_length]u8) void {
+        var leftA = d.s[0];
+        var leftB = d.s[1];
+        var leftC = d.s[2];
+        var leftD = d.s[3];
+        var leftE = d.s[4];
+
+        var rightA = d.s[0];
+        var rightB = d.s[1];
+        var rightC = d.s[2];
+        var rightD = d.s[3];
+        var rightE = d.s[4];
+
+        const words: [16]u32 = blockToWords(b);
+        var tmp: u32 = undefined;
+        var j: usize = 0;
+        while (j < 80) : (j += 1) {
+            // zig fmt: off
+            tmp = std.math.rotl(u32, leftA 
+                    +% func(j, leftB, leftC, leftD) 
+                    +% words[left_selecting_words[j]] 
+                    +% left_K[j / 16],
+                left_tmp_shift_amount[j]) +% leftE;
+            // zig fmt: on
+            leftA = leftE;
+            leftE = leftD;
+            leftD = std.math.rotl(u32, leftC, 10);
+            leftC = leftB;
+            leftB = tmp;
+
+            // zig fmt: off
+            tmp = std.math.rotl(u32, rightA 
+                    +% func(79 - j, rightB, rightC, rightD) 
+                    +% words[right_selecting_words[j]] 
+                    +% right_K[j / 16], 
+                right_tmp_shift_amount[j]) +% rightE;
+            // zig fmt: on
+            rightA = rightE;
+            rightE = rightD;
+            rightD = std.math.rotl(u32, rightC, 10);
+            rightC = rightB;
+            rightB = tmp;
         }
 
-        return res;
+        tmp = d.s[1] +% leftC +% rightD;
+        d.s[1] = d.s[2] +% leftD +% rightE;
+        d.s[2] = d.s[3] +% leftE +% rightA;
+        d.s[3] = d.s[4] +% leftA +% rightB;
+        d.s[4] = d.s[0] +% leftB +% rightC;
+        d.s[0] = tmp;
+    }
+
+    pub fn final(d: *Self, out: *[digest_length]u8) void {
+        // The buffer here will never be completely full.
+        @memset(d.buf[d.buf_len..], 0);
+
+        // Append padding bits.
+        d.buf[d.buf_len] = 0x80;
+        d.buf_len += 1;
+
+        // > 448 mod 512 so need to add an extra round to wrap around.
+        if (64 - d.buf_len < 8) {
+            d.round(d.buf[0..]);
+            @memset(d.buf[0..], 0);
+        }
+
+        // Append message length in more simple way
+        const len = (d.total_len * 8);
+        mem.writeInt(u64, d.buf[56..64], len, .little);
+
+        d.round(d.buf[0..]);
+
+        for (d.s, 0..) |s, j| {
+            mem.writeInt(u32, out[4 * j ..][0..4], s, .little);
+        }
+    }
+
+    pub fn hash(b: []const u8, out: *[digest_length]u8, options: Options) void {
+        var d = Ripemd160.init(options);
+        d.update(b);
+        d.final(out);
     }
 };
 
-fn compress(hash: *[5]u32, words: [16]u32) void {
-    const permutations: [10][16]u5 = .{
-        .{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
-        .{ 7, 4, 13, 1, 10, 6, 15, 3, 12, 0, 9, 5, 2, 14, 11, 8 },
-        .{ 3, 10, 14, 4, 9, 15, 8, 1, 2, 7, 0, 6, 13, 11, 5, 12 },
-        .{ 1, 9, 11, 10, 0, 8, 12, 4, 13, 3, 7, 15, 14, 5, 6, 2 },
-        .{ 4, 0, 5, 9, 7, 12, 2, 10, 14, 1, 3, 8, 11, 6, 15, 13 },
-        .{ 5, 14, 7, 0, 9, 2, 11, 4, 13, 6, 15, 8, 1, 10, 3, 12 },
-        .{ 6, 11, 3, 7, 0, 13, 5, 10, 14, 15, 8, 12, 4, 9, 1, 2 },
-        .{ 15, 5, 1, 3, 7, 14, 6, 9, 11, 8, 12, 2, 10, 0, 4, 13 },
-        .{ 8, 6, 4, 1, 3, 11, 15, 0, 5, 12, 2, 13, 9, 7, 10, 14 },
-        .{ 12, 15, 10, 4, 1, 5, 8, 7, 6, 2, 13, 14, 0, 3, 9, 11 },
+test "test vectors" {
+    const input = [_][]const u8{
+        "",
+        "a",
+        "abc",
+        "message digest",
+        "abcdefghijklmnopqrstuvwxyz",
+        "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+        "1234567890" ** 8,
+        "a" ** 1000000,
     };
-
-    const shifts: [10][16]u5 = .{
-        .{ 11, 14, 15, 12, 5, 8, 7, 9, 11, 13, 14, 15, 6, 7, 9, 8 },
-        .{ 7, 6, 8, 13, 11, 9, 7, 15, 7, 12, 15, 9, 11, 7, 13, 12 },
-        .{ 11, 13, 6, 7, 14, 9, 13, 15, 14, 8, 13, 6, 5, 12, 7, 5 },
-        .{ 11, 12, 14, 15, 14, 15, 9, 8, 9, 14, 5, 6, 8, 6, 5, 12 },
-        .{ 9, 15, 5, 11, 6, 8, 13, 12, 5, 12, 13, 14, 11, 8, 5, 6 },
-        .{ 8, 9, 9, 11, 13, 15, 15, 5, 7, 7, 8, 11, 14, 14, 12, 6 },
-        .{ 9, 13, 15, 7, 12, 8, 9, 11, 7, 7, 12, 7, 6, 15, 13, 11 },
-        .{ 9, 7, 15, 11, 8, 6, 6, 14, 12, 13, 5, 14, 13, 13, 7, 5 },
-        .{ 15, 5, 8, 11, 14, 14, 6, 14, 6, 9, 12, 9, 12, 5, 15, 8 },
-        .{ 8, 5, 12, 9, 12, 5, 14, 6, 8, 13, 6, 5, 15, 13, 11, 11 },
+    const output = [_][]const u8{
+        "9c1185a5c5e9fc54612808977ee8f548b2258d31",
+        "0bdc9d2d256b3ee9daae347be6f4dc835a467ffe",
+        "8eb208f7e05d987a9b044a8e98c6b087f15a0bfc",
+        "5d0689ef49d2fae572b881b123a85ffa21595f36",
+        "f71c27109c692c1b56bbdceb5b9d2865b3708dbc",
+        "12a053384a9c0c88e405a06c27dcf49ada62eb2b",
+        "b0e20b6e3116640286ed3a87a5713079b21f5189",
+        "9b752e45573d4b39f4dbd3323cab82bf63326bfb",
+        "52783243c1697bdbe16d37f97f68f08325dc1528",
     };
-
-    var left: [5]u32 = undefined;
-    var right: [5]u32 = undefined;
-    @memcpy(left[0..], hash[0..5]);
-    @memcpy(right[0..], hash[0..5]);
-
-    round(bfn1, 0, 0x00000000, permutations[0], shifts[0], &left, words);
-    round(bfn2, -1, 0x5a827999, permutations[1], shifts[1], &left, words);
-    round(bfn3, -2, 0x6ed9eba1, permutations[2], shifts[2], &left, words);
-    round(bfn4, -3, 0x8f1bbcdc, permutations[3], shifts[3], &left, words);
-    round(bfn5, -4, 0xa953fd4e, permutations[4], shifts[4], &left, words);
-
-    round(bfn5, 0, 0x50a28be6, permutations[5], shifts[5], &right, words);
-    round(bfn4, -1, 0x5c4dd124, permutations[6], shifts[6], &right, words);
-    round(bfn3, -2, 0x6d703ef3, permutations[7], shifts[7], &right, words);
-    round(bfn2, -3, 0x7a6d76e9, permutations[8], shifts[8], &right, words);
-    round(bfn1, -4, 0x00000000, permutations[9], shifts[9], &right, words);
-
-    right[3] = right[3] +% left[2] +% hash[1];
-    hash[1] = hash[2] +% left[3] +% right[4];
-    hash[2] = hash[3] +% left[4] +% right[0];
-    hash[3] = hash[4] +% left[0] +% right[1];
-    hash[4] = hash[0] +% left[1] +% right[2];
-    hash[0] = right[3];
-}
-
-fn round(
-    comptime bfn: fn (u32, u32, u32) u32,
-    comptime off: i32,
-    comptime constant: u32,
-    comptime p: [16]u5,
-    comptime shifts: [16]u5,
-    hash: *[5]u32,
-    words: [16]u32,
-) void {
-    comptime var r = off;
-    comptime var i: usize = 0;
-    inline while (i < 16) : (i += 1) {
-        hash[@mod(r + 0, 5)] = hash[@mod(r + 0, 5)] +% bfn(hash[@mod(r + 1, 5)], hash[@mod(r + 2, 5)], hash[@mod(r + 3, 5)]) +% words[p[i]] +% constant;
-        hash[@mod(r + 0, 5)] = std.math.rotl(u32, hash[@mod(r + 0, 5)], @as(u32, @intCast(shifts[i]))) +% hash[@mod(r + 4, 5)];
-        hash[@mod(r + 2, 5)] = std.math.rotl(u32, hash[@mod(r + 2, 5)], 10);
-        r -= 1;
+    for (0..input.len) |i| {
+        var expected_output: [Ripemd160.digest_length]u8 = undefined;
+        _ = try std.fmt.hexToBytes(&expected_output, output[i]);
+        var actual_output: [Ripemd160.digest_length]u8 = undefined;
+        Ripemd160.hash(input[i], &actual_output, .{});
+        try testing.expectEqualSlices(u8, &expected_output, &actual_output);
     }
 }
 
-fn bfn1(x: u32, y: u32, z: u32) u32 {
-    return x ^ y ^ z;
+test "streaming" {
+    var h = Ripemd160.init(.{});
+    var out: [Ripemd160.digest_length]u8 = undefined;
+    h.final(&out);
+    try testing.expectEqualSlices(u8, &[_]u8{
+        0x9c, 0x11, 0x85, 0xa5, 0xc5, 0xe9, 0xfc, 0x54, 0x61, 0x28,
+        0x08, 0x97, 0x7e, 0xe8, 0xf5, 0x48, 0xb2, 0x25, 0x8d, 0x31,
+    }, &out);
+
+    h = Ripemd160.init(.{});
+    h.update("abc");
+    h.final(&out);
+    try testing.expectEqualSlices(u8, &[_]u8{
+        0x8e, 0xb2, 0x08, 0xf7, 0xe0, 0x5d, 0x98, 0x7a, 0x9b, 0x04,
+        0x4a, 0x8e, 0x98, 0xc6, 0xb0, 0x87, 0xf1, 0x5a, 0x0b, 0xfc,
+    }, &out);
+
+    h = Ripemd160.init(.{});
+    h.update("a");
+    h.update("b");
+    h.update("c");
+    h.final(&out);
+    try testing.expectEqualSlices(u8, &[_]u8{
+        0x8e, 0xb2, 0x08, 0xf7, 0xe0, 0x5d, 0x98, 0x7a, 0x9b, 0x04,
+        0x4a, 0x8e, 0x98, 0xc6, 0xb0, 0x87, 0xf1, 0x5a, 0x0b, 0xfc,
+    }, &out);
 }
 
-fn bfn2(x: u32, y: u32, z: u32) u32 {
-    return (x & y) | (~x & z);
-}
+const left_selecting_words = [80]u32{
+    0, 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
+    7, 4,  13, 1,  10, 6,  15, 3,  12, 0, 9,  5,  2,  14, 11, 8,
+    3, 10, 14, 4,  9,  15, 8,  1,  2,  7, 0,  6,  13, 11, 5,  12,
+    1, 9,  11, 10, 0,  8,  12, 4,  13, 3, 7,  15, 14, 5,  6,  2,
+    4, 0,  5,  9,  7,  12, 2,  10, 14, 1, 3,  8,  11, 6,  15, 13,
+};
 
-fn bfn3(x: u32, y: u32, z: u32) u32 {
-    return (x | ~y) ^ z;
-}
+const right_selecting_words = [80]u32{
+    5,  14, 7,  0, 9, 2,  11, 4,  13, 6,  15, 8,  1,  10, 3,  12,
+    6,  11, 3,  7, 0, 13, 5,  10, 14, 15, 8,  12, 4,  9,  1,  2,
+    15, 5,  1,  3, 7, 14, 6,  9,  11, 8,  12, 2,  10, 0,  4,  13,
+    8,  6,  4,  1, 3, 11, 15, 0,  5,  12, 2,  13, 9,  7,  10, 14,
+    12, 15, 10, 4, 1, 5,  8,  7,  6,  2,  13, 14, 0,  3,  9,  11,
+};
 
-fn bfn4(x: u32, y: u32, z: u32) u32 {
-    return (x & z) | (y & ~z);
-}
+const left_tmp_shift_amount = [80]u32{
+    11, 14, 15, 12, 5,  8,  7,  9,  11, 13, 14, 15, 6,  7,  9,  8,
+    7,  6,  8,  13, 11, 9,  7,  15, 7,  12, 15, 9,  11, 7,  13, 12,
+    11, 13, 6,  7,  14, 9,  13, 15, 14, 8,  13, 6,  5,  12, 7,  5,
+    11, 12, 14, 15, 14, 15, 9,  8,  9,  14, 5,  6,  8,  6,  5,  12,
+    9,  15, 5,  11, 6,  8,  13, 12, 5,  12, 13, 14, 11, 8,  5,  6,
+};
 
-fn bfn5(x: u32, y: u32, z: u32) u32 {
-    return x ^ (y | ~z);
-}
+const right_tmp_shift_amount = [80]u32{
+    8,  9,  9,  11, 13, 15, 15, 5,  7,  7,  8,  11, 14, 14, 12, 6,
+    9,  13, 15, 7,  12, 8,  9,  11, 7,  7,  12, 7,  6,  15, 13, 11,
+    9,  7,  15, 11, 8,  6,  6,  14, 12, 13, 5,  14, 13, 13, 7,  5,
+    15, 5,  8,  11, 14, 14, 6,  14, 6,  9,  12, 9,  12, 5,  15, 8,
+    8,  5,  12, 9,  12, 5,  14, 6,  8,  13, 6,  5,  15, 13, 11, 11,
+};
 
-fn testHashEql(expected: []const u8, in: []const u8) !void {
-    const hash = Ripemd160.hash(in);
-    var hex_str: [40]u8 = undefined;
-    _ = try std.fmt.bufPrint(&hex_str, "{x}", .{std.fmt.fmtSliceHexLower(hash.bytes[0..])});
-    try std.testing.expectEqualSlices(u8, expected, hex_str[0..]);
-}
-
-test "RIPEMD-160 standard tests" {
-    try testHashEql("9c1185a5c5e9fc54612808977ee8f548b2258d31", "");
-    try testHashEql("0bdc9d2d256b3ee9daae347be6f4dc835a467ffe", "a");
-    try testHashEql("8eb208f7e05d987a9b044a8e98c6b087f15a0bfc", "abc");
-    try testHashEql("5d0689ef49d2fae572b881b123a85ffa21595f36", "message digest");
-    try testHashEql("f71c27109c692c1b56bbdceb5b9d2865b3708dbc", "abcdefghijklmnopqrstuvwxyz");
-    try testHashEql("12a053384a9c0c88e405a06c27dcf49ada62eb2b", "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq");
-    try testHashEql("b0e20b6e3116640286ed3a87a5713079b21f5189", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
-    try testHashEql("9b752e45573d4b39f4dbd3323cab82bf63326bfb", "1234567890" ** 8);
-    try testHashEql("52783243c1697bdbe16d37f97f68f08325dc1528", "a" ** 1000000);
-}
+const left_K = [5]u32{ 0x00000000, 0x5A827999, 0x6ED9EBA1, 0x8F1BBCDC, 0xA953FD4E };
+const right_K = [5]u32{ 0x50A28BE6, 0x5C4DD124, 0x6D703EF3, 0x7A6D76E9, 0x00000000 };
